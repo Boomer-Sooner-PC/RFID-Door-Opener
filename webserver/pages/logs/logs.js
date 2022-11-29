5100f81dbd275cee94e40ba08a45af349d32efcc
Boomer Sooner <68070476+Boomer-Sooner-PC@users.noreply.github.com>
2022-11-29 00:38:54 -0600

    logs

--- webserver/pages/logs/logs.js
+++ webserver/pages/logs/logs.js
@@ -9,26 +9,48 @@ function execute (data, res) {
         start = parseInt(data.split("&")[0]);
         end = parseInt(data.split("&")[1]);
         
+        dates = getDatesInRange(new Date(start * 1000 + 86400000), new Date(end * 1000));
+
         json = JSON.parse(fs.readFileSync("./data/logs.json", "utf-8"));
         keys = Object.keys(json).sort((a, b) => parseInt(a) -parseInt(b));
         
         logs = []
-        for (key of keys) {
-            key = parseInt(key);
-            if (key > start && key < end) {
-                logs.push({"time": key, "data": json[key]});
+        for (date of dates) {
+            try {
+                json = JSON.parse(fs.readFileSync(`./data/logs/${date}.json`));
+                for (key in json) {
+                    logs.push({"time": parseInt(key), "data": json[key]});
+                }
             }
+            catch (e) {}
         }
+
+        
+        console.log(logs)
         csv = "TIME,ID,NAME,STATUS";
         for (log of logs) {
-            row = `\n${new Date(log.time).toString()},${log.data.id},TEMP,${log.data.opened}}`
+            d = new Date(log.time)
+            row = `#${`${d.getMonth()}-${d.getDate()}-${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`},${log.data.id},TEMP,${log.data.opened}`
             csv+=row;
         }
-        res.writeHead(200, 'text/csv');
-        res.end(csv);
+        res.writeHead(200, 'text/html');
+        res.end(fs.readFileSync("./pages/logs/showLogs.html", 'utf-8').replace("__", csv    ));
         return;
     }
 
     res.writeHead(200, 'text/html') 
     res.end(text); // return html
+}
+
+function getDatesInRange(startDate, endDate) {
+const date = new Date(startDate.getTime());
+
+const dates = [];
+
+while (date <= endDate) {
+    d = new Date(date);
+    dates.push(`${d.getMonth()}-${d.getDate()}-${d.getFullYear()}`);
+    date.setDate(date.getDate() + 1);
+}
+return dates;
 }
--- webserver/querys/log.js
+++ webserver/querys/log.js
@@ -4,12 +4,18 @@ module.exports = {execute};
 
 function execute (password, timestamp, id, action, res) {
     if (password === process.CONFIG.masterPassword) {
-        json = JSON.parse(fs.readFileSync("./data/logs.json", "utf-8"));
+        fiN = `${d.getMonth()}-${d.getDate()}-${d.getFullYear()}`;
+        try{
+            json = JSON.parse(fs.readFileSync(`./data/logs/${fiN}.json`, "utf-8"));
+        }
+        catch (e) {
+            json = {}
+        }
         json[timestamp] = {
             "id": id,
             "opened": action === "opened" ? true : false
         }
-        fs.writeFileSync("./data/logs.json", JSON.stringify(json, null, 2));
+        fs.writeFileSync(`./data/logs/${fiN}.json`, JSON.stringify(json, null, 2));
         res.end("sucsess");
     }
     else {
